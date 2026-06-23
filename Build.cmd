5b52aa98e6aa0f23394f1feb976c2d39ea294970
DeadHand <90804577+DeadHandConspiracy@users.noreply.github.com>
2026-06-23 12:56:12 -0400

    Update Build.cmd
    Removed temporary items and added Linux make file.

--- Build.cmd
+++ Build.cmd
@@ -6,14 +6,10 @@
 @echo off
 setlocal
 
-echo [Windows] OS: %OS%
-echo [Windows] Current dir: %CD%
-
 CALL "%CD%\Windows\Build.bat" %1
 exit /b
 BATCH
 
 # Linux code starts here
-echo "[Linux] OS: $(uname -s)"
-echo "[Linux] Current dir: $(pwd)"
+make -C Linux
 exit 0
