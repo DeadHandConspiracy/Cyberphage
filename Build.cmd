52e1c72d52e557bec106615b45fc4bb69de45178
DeadHand <90804577+DeadHandConspiracy@users.noreply.github.com>
2026-07-03 16:15:35 -0400

    Add files via upload

--- Build.cmd
+++ Build.cmd
@@ -5,11 +5,43 @@
 :<<BATCH
 @echo off
 setlocal
-
 CALL "%CD%\Windows\Build.bat" %1
 exit /b
 BATCH
 
 # Linux code starts here
-make -C Linux
+
+clear
+
+# Define options using an array
+options=("Build" "Rebuild" "Clean" "Exit")
+
+# Customize the prompt (default is #?)
+PS3="Select an option (1-4): "
+
+# Display menu and handle selection
+select opt in "${options[@]}"; do
+    case $opt in
+        "Build")
+            echo "You selected ${options[0]}"
+			current_dir=$(pwd)
+			make -f $current_dir/Linux/makefile test
+			break
+            ;;
+        "Rebuild")
+            echo "You selected ${options[1]}"
+            ;;
+        "Clean")
+            echo "You selected ${options[2]}"
+            ;;
+        "Exit")
+            echo "Exiting..."
+            break
+            ;;
+        *)
+            echo "Invalid selection. Please try again."
+            ;;
+    esac
+done
+
 exit 0
